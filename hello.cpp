55c508311247f4c0f76fdf4ff7a121988c7765aa
Stanislav Furmavnin <118751660+saintninja@users.noreply.github.com>
2026-03-25 12:44:10 +0000

    fix: hello world exclamation added

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::cout << "First line" << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!!!!!!!!" << std::endl;
     std::cout << "Photoshop" << std::endl;
     std::cout << "Last line" << std::endl;
     return 0;
