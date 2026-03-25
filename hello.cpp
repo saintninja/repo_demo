992c5e850cfb6b1444718b5337a4e9b55df161d7
saintninja@gmail <stas.saintninja@gmail.com>
2026-03-25 15:37:13 +0300

    feat: first line added

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
+    std::cout << "First line" << std::endl;
     std::cout << "Hello, world!" << std::endl;
     std::cout << "Photoshop" << std::endl;
     return 0;
